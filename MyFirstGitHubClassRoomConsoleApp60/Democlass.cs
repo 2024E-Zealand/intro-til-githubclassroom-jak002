df597198e58f1473c5cc7b2ef0093ff0f22e301e
Jacob Sejer Kristoffersen <jak002@edu.zealand.dk>
2024-08-27 12:49:32 +0200

    hej martin :D

---
+++ MyFirstGitHubClassRoomConsoleApp60/Democlass.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    public class Democlass
+    {
+
+        public int Numberstuff {  get; set; }
+        public string? Wordstuff { get; set; }
+        public double Decimalstuff { get; set; }
+
+        public Democlass()
+        {
+            Numberstuff = 0;
+            Wordstuff = "Empty";
+            Decimalstuff = 0;
+        }
+
+        public Democlass(int number, double deci, string word)
+        {
+            Numberstuff = number;
+            Wordstuff = word;
+            Decimalstuff = deci;
+        }
+
+        public override string ToString()
+        {
+            return $"{Wordstuff}: {Numberstuff} & {Decimalstuff}";
+        }
+    }
+}
--- MyFirstGitHubClassRoomConsoleApp60/Program.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using MyFirstGitHubClassRoomConsoleApp60;
+
+Democlass d1 = new();
+Democlass d2 = new(4, 2.6, "Steve");
+
+Console.WriteLine(d1);
+Console.WriteLine(d2);
